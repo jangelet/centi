a858516ed7f5ca65d4cbd67b2af7e55f3b862452
Joseph Angeletti <jangelet@gmail.com>
2018-06-25 10:24:18 -0400

    fixed views

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
 const Vibrant = require('node-vibrant');
+
+//set up template engine
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'views')));
 
 var colors;
 
@@ -9,6 +16,10 @@ let v = new Vibrant('/Users/JANG3LET/Desktop/IMG_2303.jpg');
 //gets RGB palette values from vibrant object, assigns to colors
 v.getPalette((err, palette) => (colors = palette));
 
-app.get('/', (req, res) => res.(colors));
+app.get('/', (req, res) => res.render('main', { color: colors }));
+
+// app.post('/', (reg, res) => res.send({ color: colors }));
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => console.log('Centi listening on port 3000!'));
